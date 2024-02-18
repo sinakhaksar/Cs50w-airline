041f23c21eca7077adc9ec1af4e8944b977dca58
sina <sinakhaksar3@gmail.com>
2024-02-19 02:44:19 +0330

    pooof

--- flights/urls.py
+++ flights/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns =[
-    
-]
+    path('',views.index, name='index'),
+    path('<int:flight_id>',views.flight, name='flight' )
+    ]
--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+# i need flights
+from .models import Flight
 # Create your views here.
+
+
+def index(request):
+    return render (request,'flights/index.html', {
+        'flights': Flight.objects.all()
+    } )
+
+
+def flight(request, flight_id):
+
+    flight = Flight.objects.get(pk=flight_id)
+
+    return render (request, 'flights/flight.html', {
+        'flight': flight
+    })
