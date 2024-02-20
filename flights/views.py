1a32001174048a37480f03ea36b5cf901f5d6956
sina <sinakhaksar3@gmail.com>
2024-02-20 19:04:52 +0330

    added users app and completed it ... airline Project is on a decent level know

--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # i need flights
-from .models import Flight
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Flight, Passenger
 # Create your views here.
 
 
@@ -17,5 +19,15 @@ def flight(request, flight_id):
 
     return render (request, 'flights/flight.html', {
         'flight': flight,
-        'passengers': flight.passengers.all()
+        'passengers': flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+    
